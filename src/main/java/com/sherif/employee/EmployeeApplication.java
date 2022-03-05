661d9ec741e2ccc0880a633a93491b4c045e1c9f
Sherif Hamad <sherif.hamad@audibene.de>
2022-03-05 20:16:13 +0100

    Fix eureka client settings

--- src/main/java/com/sherif/employee/EmployeeApplication.java
+++ src/main/java/com/sherif/employee/EmployeeApplication.java
@@ -3,6 +3,7 @@ package com.sherif.employee;
 import com.sherif.employee.vo.ResponseTemplateVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +18,8 @@ public class EmployeeApplication {
 
 
 	@Bean
-	 RestTemplate restTemplate() {
+	@LoadBalanced
+	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
